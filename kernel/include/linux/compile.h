bfdd53c3b961d3049db79e596b2e2873511d4e57
Keiran <keiran@darkforestgroup.com>
2011-12-18 14:33:23 +0000

    added gitignore and interactive CPU governor

---
+++ kernel/include/linux/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 5 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#5 PREEMPT Fri Dec 16 21:22:22 GMT 2011"
+#define LINUX_COMPILE_TIME "21:22:22"
+#define LINUX_COMPILE_BY "keiran"
+#define LINUX_COMPILE_HOST "keiran"
+#define LINUX_COMPILE_DOMAIN
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2010q1-188) "
--- kernel/include/linux/utsrelease.h
+++ kernel/include/linux/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "2.6.32.9_xD-MIUI"
+#define UTS_RELEASE "2.6.32.9-perf"
